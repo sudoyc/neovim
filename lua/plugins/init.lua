52e274ece2fade17a3153524af240ce569797ad2
ycyc <3113929638@qq.com>
2025-11-05 20:01:44 +0800

    .

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,26 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
+    opts = {
+      style = "storm",
+      plugins = {
+        -- enable all plugins when not using lazy.nvim
+        -- set to false to manually enable/disable plugins
+        all = package.loaded.lazy == nil,
+        -- uses your plugin manager to automatically enable needed plugins
+        -- currently only lazy.nvim is supported
+        auto = true,
+        flash = true,
+        -- add any plugins here that you want to enable
+        -- for all possible plugins, see:
+        --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+        -- telescope = true,
+      },
+    },
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    lazy = true,
   },
   {
     'wakatime/vim-wakatime',
